8b96c13ce799f076d002660d09807926de6baa1d
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-03-28 20:13:33 +0900

    Design: US 페이지 menu UI 구현

--- src/components/us/menu/UsMenu.tsx
+++ src/components/us/menu/UsMenu.tsx
@@ -1,13 +1,57 @@
-import { useState } from "react";
+import { forwardRef } from "react";
 import * as S from "./styled";
+import back from "../../../assets/img/us/menu/back.svg";
+import edit from "../../../assets/img/us/menu/edit.svg";
+import theme from "../../../assets/img/us/menu/theme.svg";
+import shopping from "../../../assets/img/us/menu/shopping.svg";
+import send from "../../../assets/img/us/menu/send.svg";
+import logout from "../../../assets/img/us/menu/logout.svg";
+import quit from "../../../assets/img/us/menu/quit.svg";
 
-const UsMenu = () => {
-  return (
-    <S.MenuContainer>
-      <S.MenuSection>메뉴 항목 1</S.MenuSection>
-      <S.MenuSection>메뉴 항목 2</S.MenuSection>
-    </S.MenuContainer>
-  );
-};
+interface UsMenuProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const UsMenu = forwardRef<HTMLDivElement, UsMenuProps>(
+  ({ isOpen, onClose }, ref) => {
+    return (
+      <S.MenuContainer $isOpen={isOpen} ref={ref}>
+        <S.Back src={back} onClick={onClose} />
+        <S.MenuSectionBox>
+          <S.MenuSection>
+            <S.Img src={edit} />
+            <S.Text>회원정보 수정</S.Text>
+          </S.MenuSection>
+          <S.Line />
+          <S.MenuSection>
+            <S.Img src={theme} />
+            <S.Text>어스 테마 바꾸기</S.Text>
+          </S.MenuSection>
+          <S.Line />
+          <S.MenuSection>
+            <S.Img src={shopping} />
+            <S.Text>구매목록</S.Text>
+          </S.MenuSection>
+          <S.Line />
+          <S.MenuSection>
+            <S.Img src={send} />
+            <S.Text>문의하기</S.Text>
+          </S.MenuSection>
+          <S.Line />
+          <S.MenuSection>
+            <S.Img src={logout} />
+            <S.Text>로그아웃</S.Text>
+          </S.MenuSection>
+          <S.Line />
+          <S.MenuSection>
+            <S.Img src={quit} />
+            <S.Text>탈퇴</S.Text>
+          </S.MenuSection>
+        </S.MenuSectionBox>
+      </S.MenuContainer>
+    );
+  }
+);
 
 export default UsMenu;
--- src/pages/UsPage.tsx
+++ src/pages/UsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styled from "styled-components";
 import UsRanking from "../components/us/UsRanking";
 import UsStep from "../components/us/UsMain";
@@ -9,43 +9,80 @@ import menu from "../assets/img/us/menu.svg";
 
 const Layout = styled.div`
   font-family: Pretendard;
-  display: flex;
-  flex-direction: column;
   padding: 0 10px;
   max-height: calc(100vh - 78px);
-  overflow-y: auto;
+  position: relative;
+  overflow: hidden;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const ContentWrapper = styled.div`
+  height: 100%;
+  overflow-y: auto; /* 컨텐츠만 스크롤 */
+  padding: 0 10px;
   &::-webkit-scrollbar {
     display: none;
   }
 `;
 
 const Menu = styled.img`
-  z-index: 1000;
+  z-index: 10;
   width: 24px;
   height: 24px;
   flex-shrink: 0;
-  margin-top: 10px;
-  margin-bottom: -40px;
+  margin: 20px 100px 0 -100px;
   cursor: pointer;
 `;
 
 // 컴포넌트 타입 명시 React.FC
 const UsPage: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const menuButtonRef = useRef<HTMLImageElement>(null);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  // 외부 클릭 감지
+  const handleClickOutside = (e: MouseEvent) => {
+    if (
+      menuRef.current &&
+      !menuRef.current.contains(e.target as Node) &&
+      menuButtonRef.current &&
+      !menuButtonRef.current.contains(e.target as Node)
+    ) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
   return (
     <>
       <Layout>
-        <Menu src={menu} onClick={toggleMenu} />
-        {isMenuOpen && <UsMenu />}
-        <UsRanking />
-        <UsStep />
-        <UsBottom />
-        <UsMarket />
+        {/* 메뉴 (항상 최상위 레이어) */}
+        {isMenuOpen && (
+          <UsMenu
+            isOpen={isMenuOpen}
+            onClose={() => setIsMenuOpen(false)}
+            ref={menuRef}
+          />
+        )}
+
+        {/* 기존 Us Page */}
+        <ContentWrapper>
+          <Menu ref={menuButtonRef} src={menu} onClick={toggleMenu} />
+          <UsRanking />
+          <UsStep />
+          <UsBottom />
+          <UsMarket />
+        </ContentWrapper>
       </Layout>
     </>
   );
