4a1f13ae4a94e1c01aed31a63903abfaefc4e109
sem201 <sem201@naver.com>
2025-04-04 17:06:33 +0900

    test

--- src/pages/SharePage.tsx
+++ src/pages/SharePage.tsx
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-import CardComponent from "@components/share/feed/CardComponent";
-import ShareHeader from "@components/share/feed/ShareHeader";
-=======
 import CardComponent from "@components/share/CardComponent";
 import ShareHeader from "@components/share/ShareHeader";
-import styled from "styled-components";
->>>>>>> d0727695a11ed4b92a63d32d568923f03e87f9f2
 
 const SharePage = () => {
   return (
