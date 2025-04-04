233a809c2e4c13e4eae3ff05cec87aae95c4809c
pedro0527 <sooyong0527@naver.com>
2025-04-04 14:22:31 +0900

    Design : 조인 설명 페이지 UI 구현

---
+++ src/pages/joinPage/JoinExplain.styled.ts
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+
+interface ContentProps {
+    step: number;
+}  
+
+export const Wrapper = styled.section`
+    width: 100%;
+    height: 95vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Title = styled.div`
+    color: ${({ theme }) => theme.colors.primaryColor};
+    text-align: center;
+    font-family: Pretendard;
+    font-size: clamp(16px, 3vw, 20px);
+    font-style: normal;
+    font-weight: 800;
+    line-height: 150%; /* 30px */
+    letter-spacing: -0.8px;
+
+    span {
+        color: ${({ theme }) => theme.colors.red};
+    }
+`;
+
+export const Image = styled.div`
+    display: flex;
+    margin: 40px 0 30px 0;
+`;
+
+export const Content = styled.div<ContentProps>`
+    color: ${({ theme }) => theme.colors.gray5};
+    font-family: Pretendard;
+    font-size: clamp(16px, 3vw, 20px);
+    font-style: normal;
+    font-weight: 400;
+    /* line-height: 150%; */
+    letter-spacing: -0.8px;
+    text-align: ${({ step }) => (step === 3 ? "left" : "inherit")};
+
+    span {
+        color: ${({ theme, step }) => 
+        step === 3 ? theme.colors.primaryColor : theme.colors.gray5};
+        font-weight: 700;
+    }
+`;
+
+export const BtnContainer = styled.div`
+    display: flex;
+    margin-top: 112px;
+    gap: 100px;
+`
+
+export const Back = styled.img`
+
+`;
+
+export const Front = styled.img`
+
+`;
+
+export const ConfirmContainer = styled.div`
+    display: flex;
+    width: 65%;
+    justify-content: flex-end;
+    margin-top: 10px;
+`
+
+export const confirm = styled.img`
+
+`;
