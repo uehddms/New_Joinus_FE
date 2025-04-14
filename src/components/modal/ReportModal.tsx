45285597981b37f269636784b05339fd306fa214
sem201 <sem201@naver.com>
2025-04-15 00:55:10 +0900

    Design : 신고하기 모달 창 완성

--- src/components/modal/ReportModal.tsx
+++ src/components/modal/ReportModal.tsx
@@ -1,4 +1,46 @@
-const ReportModal = () => {
-  return <></>;
+import styled from "styled-components";
+import * as S from "./modal.styled";
+import report from "@assets/icons/report.svg";
+
+interface ReportModalProps {
+  onClose: () => void;
+  onConfirm: () => void;
+}
+
+const ReportModal = ({ onClose, onConfirm }: ReportModalProps) => {
+  return (
+    <Wrapper>
+      <S.ReportModalWrapper>
+        <S.ReportModalTextContainer>
+          <img src={report} alt="신고" />
+          <p>해당 게시물을 신고하시겠습니까?</p>
+        </S.ReportModalTextContainer>
+        <div
+          style={{
+            display: "flex",
+            gap: "10px",
+            height: "50%",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <S.ReportModalButton onClick={onConfirm}>네</S.ReportModalButton>
+          <S.ReportModalButton bgcolor="primaryColor" onClick={onClose}>
+            아니오
+          </S.ReportModalButton>
+        </div>
+      </S.ReportModalWrapper>
+    </Wrapper>
+  );
 };
 export default ReportModal;
+
+const Wrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 10;
+`;
--- src/components/share/detail/CommentBox.tsx
+++ src/components/share/detail/CommentBox.tsx
@@ -1,9 +1,16 @@
 import styled from "styled-components";
 import * as S from "./detail.styled";
+import send from "@assets/icons/send.svg";
 const CommentBox = () => {
   return (
     <Wrapper>
-      <S.InputComment placeholder="댓글 쓰기" />
+      <S.CommentInputContainer>
+        <S.InputComment placeholder="댓글 쓰기" />
+        <button style={{ cursor: "pointer" }}>
+          <img src={send} alt="보내기" />
+        </button>
+      </S.CommentInputContainer>
+
       <S.AddedComment>댓글쓰기</S.AddedComment>
       <S.AddedComment>댓글쓰기- 댓글쓰기</S.AddedComment>
       <S.AddedComment>댓글쓰기</S.AddedComment>
