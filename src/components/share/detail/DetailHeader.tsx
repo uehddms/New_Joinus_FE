f9a75b40e4ef9309b337719cee946c6c07bc4cb2
sjae_o <38523683+sem201@users.noreply.github.com>
2025-04-15 00:56:34 +0900

    Design/#17 (#9)
    * 💄 [Design] -  피드 디테일 페이지를 만들었습니다 (#26)
    
    * Chore : tsconfig.app.json 수정 -> 이제 @사용해도 vscode상에서 빨간줄이 안생깁니다..
    
    * Design : 공유 페이지 메인 화면 완성
    
    * Rename: cardpng 이름 수정
    
    * design/#17 : 피드 하단 부 투명도 처리
    
    * Design :  피드 디테일 페이지를 만들었습니다
    
    * Design/#17 (#7)
    
    * 💄 [Design] - 공유 페이지 메인 컴포넌트 디자인 (2024-line4-earth#22)  (#23)
    
    * Rename: 이미지, 아이콘 위치 수정 / Design: 회원정보 수정 페이지, 문의하기 페이지
    
    * Design: common button 수정
    
    * Design : 조인 프레임 선택 페이지 UI 구현>
    
    * Design : share 페이지 소형 기기에서 글자 잘리는거 수정
    
    * Design : 신고하기 모달 창 완성

--- src/components/share/detail/DetailHeader.tsx
+++ src/components/share/detail/DetailHeader.tsx
@@ -1,11 +1,13 @@
 import goLeft from "@assets/icons/goLeft.png";
-
+import { Link } from "react-router-dom";
 import * as S from "./detail.styled";
 
 const DetailHeader = () => {
   return (
     <S.Wrapper>
-      <img src={goLeft} alt="뒤로 가기 버튼" />
+      <Link to="/share">
+        <img src={goLeft} alt="뒤로 가기 버튼" />
+      </Link>
       <p>조인어스</p>
     </S.Wrapper>
   );
