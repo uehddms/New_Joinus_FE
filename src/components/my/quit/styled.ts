f793c89e6333ad73a28c0eb6773c0ae9f9222e8d
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-04-20 16:59:28 +0900

    Feat: MyQuit 페이지 UI 구현 완료

--- src/components/my/quit/styled.ts
+++ src/components/my/quit/styled.ts
@@ -1,99 +1,46 @@
 import styled from "styled-components";
 
-/* MyContainer */
-export const MyPageContainer = styled.div`
+export const MyQuitContainer = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
   width: 100%;
+  height: calc(100vh - 200px);
   max-width: 31.25rem;
   margin: 0 auto;
   padding: 1.25rem;
   background-color: white;
 `;
 
-export const MyContainer = styled.div`
+export const ModalContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  background-color: rgba(0, 0, 0, 0.4);
   display: flex;
-  flex-direction: column;
-  flex: 1;
-  padding-bottom: 1.25rem;
-  width: calc(100% - 2.5rem);
-`;
-
-export const EditBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.75rem;
-  padding: 1.875rem;
-  height: 28.125rem;
-`;
-
-export const UsernameTitle = styled.div`
-  color: ${({ theme }) => theme.colors.gray4};
-  font-size: 1.0625rem;
-  font-weight: 700;
-  align-self: flex-start;
-  text-align: left;
-`;
-
-export const UsernameCurrentBox = styled.div`
-  width: 100%;
-  height: 3.125rem;
-  display: flex;
+  justify-content: center;
   align-items: center;
-  padding: 0 1.25rem;
-  border-radius: 1.5625rem;
-  border: 1px solid ${({ theme }) => theme.colors.gray2};
-  background: white;
+  z-index: 1000;
+`;
+
+export const QuitNotice = styled.div`
   color: ${({ theme }) => theme.colors.gray5};
-  font-size: 1rem;
-  margin-bottom: 1.5rem;
-`;
-
-export const UsernameInputBox = styled.input`
-  width: 100%;
-  height: 3.125rem;
-  padding: 0 1.25rem;
-  border-radius: 1.5625rem;
-  border: 1px solid ${({ theme }) => theme.colors.gray2};
-  background: white;
-  color: ${({ theme }) => theme.colors.gray5};
-  font-size: 1rem;
-  font-weight: 400;
-  outline: none;
-
-  &::placeholder {
-    color: ${({ theme }) => theme.colors.gray3};
-  }
-`;
-
-export const ValidationMessage = styled.div<{ isError: boolean }>`
-  text-align: right;
-  font-size: 0.75rem;
-  margin-top: -0.3125rem;
-  margin-left: 0.5rem;
-  color: ${(props) => (props.isError ? "#DB7575" : "#73C56A")};
-`;
-
-export const ButtonSection = styled.div``;
-
-/* MyQuit.jsx */
-export const Quit_text = styled.h2`
-  margin-bottom: 0.3125rem;
-  color: var(--Gray5, #2e302d);
+  text-align: center;
   font-family: Pretendard;
-  font-size: 1.25rem;
+  font-size: 1.2rem;
   font-style: normal;
   font-weight: 700;
-  line-height: normal;
+  line-height: 175%;
   letter-spacing: -0.05rem;
 `;
-export const Quit_Warning = styled.p`
-  margin-bottom: 1.25rem;
-  color: var(--Negative-Error-Cancel, #f66466);
-  font-family: Pretendard;
-  font-size: 0.75rem;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
-  letter-spacing: -0.03rem;
+
+export const QuitBtnContainer = styled.div`
+  margin-top: 2.25rem;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
