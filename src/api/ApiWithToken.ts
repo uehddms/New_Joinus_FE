2ce49faf0073d7d4986614a1495ef706ce776be4
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-06-13 21:34:32 +0900

    Fix: 인터셉터로 토큰을 항상 동적으로 불러오기 (최신 토큰 불러올 수 있게 오류 수정)

--- src/api/ApiWithToken.ts
+++ src/api/ApiWithToken.ts
@@ -5,6 +5,19 @@ export const ApiwithToken = axios.create({
   baseURL: import.meta.env.VITE_SERVER_API,
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Token ${Cookies.get("access_token")}`,
   },
 });
+
+// 인터셉터로 토큰을 항상 동적으로 불러오기
+ApiwithToken.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("access_token");
+    if (token) {
+      config.headers.Authorization = `Token ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
