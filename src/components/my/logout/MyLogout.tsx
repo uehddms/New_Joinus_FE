a90ccda985ef2594f67d4a7fd7a8b689481e08fa
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-06-19 16:11:31 +0900

    Feat: 로그아웃 연동

--- src/components/my/logout/MyLogout.tsx
+++ src/components/my/logout/MyLogout.tsx
@@ -1,17 +1,45 @@
 import { useNavigate } from "react-router-dom";
 import * as S from "./styled";
 import YesNoModal from "../../modal/YesNoModal";
+import { logoutApi } from "@api/my/LogoutApi";
+import Cookies from "js-cookie";
 
 const MyLogout = () => {
   const navigate = useNavigate();
 
+  const postLogout = async () => {
+    const token = Cookies.get("access_token");
+    if (!token) {
+      alert("로그인이 되어 있지 않습니다.");
+      return; // 토큰 없으면 아무 작업도 하지 않음
+    }
+
+    try {
+      const data = await logoutApi.postLogout();
+
+      if (data?.message) {
+        alert(data.message);
+        Cookies.remove("access_token"); // 쿠키 삭제
+        navigate("/");
+      } else if (data?.detail) {
+        alert(data.detail); // 백엔드에서 detail 응답 시
+      } else {
+        alert("로그아웃에 실패했습니다."); // 응답이 비정상적일 때
+      }
+    } catch (error) {
+      alert("알 수 없는 오류가 발생했습니다.");
+      console.error(error);
+    }
+  };
+
   const handleYes = () => {
-    navigate("/");
+    postLogout();
   };
 
   const handleNo = () => {
     navigate("/us");
   };
+
   return (
     <S.MyLogoutContainer>
       <YesNoModal
