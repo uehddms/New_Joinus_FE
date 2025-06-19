b79b87b5539a715e54ef0242b657ce4990cd8942
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-06-19 16:11:17 +0900

    Feat: 로그아웃 api 구현

---
+++ src/api/my/LogoutApi.tsx
@@ -0,0 +1,20 @@
+import { ApiwithToken } from "@api/ApiWithToken";
+
+export type logoutType = {
+  message?: string;
+  detail?: string;
+};
+
+export const logoutApi = {
+  postLogout: async () => {
+    try {
+      const response = await ApiwithToken.post("/users/logout/", {});
+      console.log("로그아웃 데이터:", response.data);
+
+      return response.data;
+    } catch (error) {
+      console.error("로그아웃 데이터 post 실패:", error);
+      return error;
+    }
+  },
+};
