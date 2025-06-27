cfb7ebb7bf0e9d4e454c05371ad174affd77f419
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-06-28 01:23:21 +0900

    Feat: 아이템 구매, 다운로드 api 구현

--- src/api/market/ItemDetailApi.tsx
+++ src/api/market/ItemDetailApi.tsx
@@ -10,6 +10,10 @@ export type itemDetailType = {
   button_text: string;
 };
 
+export type itemDetailPostType = {
+  message: number;
+};
+
 export const itemDetailApi = {
   getItemDetail: async (id: number) => {
     try {
@@ -20,6 +24,35 @@ export const itemDetailApi = {
     } catch (error) {
       console.error("item detail 데이터 받아오기 실패:", error);
       return [];
+    }
+  },
+
+  // 아이템 구매
+  postItemDetail: async (id: number, postItemId: { item: number }) => {
+    try {
+      const response = await ApiwithToken.post(
+        `market/item/${id}/`,
+        postItemId
+      );
+      // console.log("item detail post 데이터:", response.data);
+
+      return response.data;
+    } catch (error) {
+      console.error("item detail 데이터 post 실패:", error);
+      return [];
+    }
+  },
+
+  // 아이템 다운로드
+  downloadItem: async (id: number) => {
+    try {
+      const response = await ApiwithToken.get(`market/item/${id}/download/`);
+      // console.log("item detail download 데이터: ", response.data);
+
+      return response.data;
+    } catch (error) {
+      console.error("다운로드 실패:", error);
+      throw error;
     }
   },
 };
