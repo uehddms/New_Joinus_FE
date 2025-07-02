58b57d89109c51f9fb9b65a0736b840d9b48d550
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-07-03 01:24:04 +0900

    Setting: 로컬호스트에서 https 실행 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import fs from "fs";
+import fs from "fs";
 import path from "path";
 
 // 로컬 호스트에서 https 실행
@@ -17,13 +17,13 @@ export default defineConfig({
       "@api": path.resolve(__dirname, "src/api"),
     },
   },
-  // server: {
-  //   https: {
-  //     key: fs.readFileSync(path.resolve(__dirname, "localhost+2-key.pem")),
-  //     cert: fs.readFileSync(path.resolve(__dirname, "localhost+2.pem")),
-  //   },
-  //   port: 5173,
-  // },
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "localhost-key.pem")),
+      cert: fs.readFileSync(path.resolve(__dirname, "localhost.pem")),
+    },
+    port: 5173,
+  },
 });
 
 // 배포 서버에서 https 실행
